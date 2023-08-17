4b7455f5fa647a0cea0f357d18ec92fc2f700d16
Omotola Awokunle <omotolaawokunle@gmail.com>
2023-08-17 12:46:27 +0100

    Set up factories for admins and users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -18,10 +18,15 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'email' => fake()->email(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$hXwPIp6/Z7u35f3X5u7vveGC6D8m1DKTfqhXyE2okjq4MlYfcIA9y',
+            'avatar' => null,
+            'address' => fake()->address(),
+            'phone_number' => fake()->phoneNumber(),
+            'is_marketing' => false,
             'remember_token' => Str::random(10),
         ];
     }
@@ -33,6 +38,18 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the user is an admin.
+     */
+    public function isAdmin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_admin' => true,
+            'email' => 'admin@buckhill.co.uk',
+            'password' => '$2y$10$LVobo5fQdtRv0RM01Rri7eh91KMjoPbLZD6roICVsrFKG/js0Bwo.'
         ]);
     }
 }
