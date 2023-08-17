8c3137c3b303a1560879ab9f0037f11daf20e345
Omotola Awokunle <omotolaawokunle@gmail.com>
2023-08-17 13:39:52 +0100

    Overwrite retrieveById function for jwt auth class

--- app/Services/Auth/Traits/JwtAuthenticable.php
+++ app/Services/Auth/Traits/JwtAuthenticable.php
@@ -17,4 +17,16 @@ trait JwtAuthenticable
             'user_uuid' => $this->uuid,
         ], config('jwt.key.private'), 'RS256');
     }
+    /**
+     * Retrieve a user by their unique identifier
+     * @param mixed $identifier
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveById($identifier)
+    {
+        if (static::where('uuid', $identifier)->exists()) {
+            return static::where('uuid', $identifier)->first();
+        }
+        return null;
+    }
 }
