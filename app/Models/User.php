93d9524061fcb38d4d006499fc3102e1471c9b10
Omotola Awokunle <omotolaawokunle@gmail.com>
2023-08-17 12:46:02 +0100

    Add guarded property 'is_admin' for User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,8 @@ class User extends Authenticatable
         'last_login_at',
     ];
 
+    protected $guarded = ['is_admin'];
+
     /**
      * The attributes that should be hidden for serialization.
      *
