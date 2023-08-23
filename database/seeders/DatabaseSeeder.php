a42dd56b2addde39a877a0b065240c3f6a8c052c
Omotola Awokunle <omotolaawokunle@gmail.com>
2023-08-23 09:06:17 +0100

    add category factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             UserSeeder::class,
             OrderStatusSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
