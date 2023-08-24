138ffd9ee7a8edc56dca6b689cc06e6596080e8d
Omotola Awokunle <omotolaawokunle@gmail.com>
2023-08-25 00:31:51 +0100

    Add invoice package and files

---
+++ resources/lang/vendor/invoices/en/invoice.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'invoice'                => 'Invoice',
+    'serial'                 => 'Serial No.',
+    'date'                   => 'Invoice date',
+    'seller'                 => 'Seller',
+    'buyer'                  => 'Customer Details',
+    'address'                => 'Address',
+    'code'                   => 'Code',
+    'vat'                    => 'VAT code',
+    'phone'                  => 'Phone',
+    'id'                     => 'ID',
+    'title'                  => 'Item Name',
+    'description'            => 'description',
+    'units'                  => 'Units',
+    'quantity'               => 'Quantity',
+    'price'                  => 'Price',
+    'unit_price'             => 'Unit Price',
+    'discount'               => 'Discount',
+    'tax'                    => 'Tax',
+    'sub_total'              => 'Sub total',
+    'total_discount'         => 'Total discount',
+    'taxable_amount'         => 'Taxable amount',
+    'total_taxes'            => 'Total taxes',
+    'tax_rate'               => 'Tax rate',
+    'total_amount'           => 'Total amount',
+    'pay_until'              => 'Please pay until',
+    'amount_in_words'        => 'Amount in words',
+    'amount_in_words_format' => '%s %s and %s %s',
+    'notes'                  => 'Notes',
+    'shipping'               => 'Delivery fee',
+    'paid'                   => 'Paid',
+    'due'                    => 'Due',
+];
