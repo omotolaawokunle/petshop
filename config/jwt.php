ca9f7adf9de26ba328d66a6d9f6ea39310070cbc
Omotola Awokunle <omotolaawokunle@gmail.com>
2023-08-25 00:33:21 +0100

    Refactored jwt config file

--- config/jwt.php
+++ config/jwt.php
@@ -2,7 +2,7 @@
 
 return [
     'key' => [
-        'public' => file_get_contents(env('JWT_PUBLIC_KEY')),
-        'private' => file_get_contents(env('JWT_PRIVATE_KEY')),
+        'public' => file_get_contents(storage_path(env('JWT_PUBLIC_KEY'))),
+        'private' => file_get_contents(storage_path(env('JWT_PRIVATE_KEY'))),
     ]
 ];
